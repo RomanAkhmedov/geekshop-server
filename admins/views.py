fab603531508e066231f5682f6bc73f9292683ee
Your Name <rakhmedov18@gmail.com>
2021-06-19 12:52:31 +0300

    added RECOVER for admin users

--- admins/views.py
+++ admins/views.py
@@ -50,3 +50,10 @@ def admin_users_delete(request, id):
     user.is_active = False
     user.save()
     return HttpResponseRedirect(reverse('admins:admin_users'))
+
+
+def admin_users_recover(request, id):
+    user = User.objects.get(id=id)
+    user.is_active = True
+    user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
